f80220c45fc8c23eedb6e241e14861811c3fd664
divyaV281 <v.divya282001@gmail.com>
2024-11-03 11:08:36 +0530

    local storage

--- todolist.js
+++ todolist.js
@@ -3,18 +3,21 @@ const ol = document.querySelector('.taskList')
 const input = document.querySelector('.inputValue')
 
 const addTaskHandler =() => {
-    // console.log(e.target);
+    if(input.value === ''){
+        alert('Enter the task...')
+    } else {
+        let li = document.createElement('li')
+        li.innerHTML = input.value
+        ol.appendChild(li)
 
-    let li = document.createElement('li')
-    li.innerHTML = input.value
-    ol.appendChild(li)
+        let removeBtn = document.createElement('button')
+            removeBtn.setAttribute('class', 'btn btn-danger removeBtn')
+        let btnText = document.createTextNode('Remove')
+        removeBtn.appendChild(btnText)
+        li.appendChild(removeBtn)
+    }
     input.value = ''
-
-    let removeBtn = document.createElement('button')
-        removeBtn.setAttribute('class', 'btn btn-danger removeBtn')
-    let btnText = document.createTextNode('Remove')
-    removeBtn.appendChild(btnText)
-    li.appendChild(removeBtn)
+    updateTask()
 }
 
 const removeTaskHandler = (e) => {
@@ -23,6 +26,7 @@ const removeTaskHandler = (e) => {
     
     if(e.target.tagName === 'BUTTON'){
         e.target.parentElement.remove()
+    updateTask()
     }
 }
 
@@ -31,3 +35,14 @@ addBtn.addEventListener('click', addTaskHandler)
 
 // to remove the task
 ol.addEventListener('click', removeTaskHandler)
+
+// Local storage
+function updateTask(){
+    localStorage.setItem('task', ol.innerHTML)
+}
+
+// getting the task from local storage
+function getTask(){
+    ol.innerHTML = localStorage.getItem('task')
+}
+getTask()
